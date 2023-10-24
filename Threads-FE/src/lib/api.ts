d2822cb7fe3898072a7538c6796cd59e7f1e28e1
AnggaNaa <angga.ardiansyah056@gmail.com>
2023-10-24 15:59:48 +0700

    fix: code

--- Threads-FE/src/lib/api.ts
+++ Threads-FE/src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  // baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://xtwitter-production.up.railway.app/api/v1",
 });
 
 export function setAuthToken(token: string) {
