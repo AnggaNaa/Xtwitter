65936be0df164e46b0eff5b1c07a6c364254940c
AnggaNaa <angga.ardiansyah056@gmail.com>
2023-10-24 08:28:14 +0700

    fix: code

--- Threads-BE/src/data-source.ts
+++ Threads-BE/src/data-source.ts
@@ -2,13 +2,27 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Thread } from "./entities/Thread";
 
+// export const AppDataSource = new DataSource({
+//   type: "postgres",
+//   host: "localhost",
+//   port: 5432,
+//   username: "postgres",
+//   password: "200799",
+//   database: "threads",
+//   synchronize: true,
+//   logging: false,
+//   entities: ["src/entities/*.ts"],
+//   migrations: ["src/migrations/*.ts"],
+//   subscribers: [],
+// });
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
+  host: "containers-us-west-90.railway.app",
+  port: 7774,
   username: "postgres",
-  password: "200799",
-  database: "threads",
+  password: "cJKKIP20PIRvvXVbjY1G",
+  database: "railway",
   synchronize: true,
   logging: false,
   entities: ["src/entities/*.ts"],
